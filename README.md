d96bdd1d191a1e4525da5a98cbcb137450c83709
Geindreau <geindreau@DESKTOP-P4JNT9J>
2022-11-30 12:38:41 +0100

    add challenge

---
+++ README.md
@@ -0,0 +1,25 @@
+## Ceci est une de mes proposition dans le cadre du DataChallenge suivant
+
+- <https://challengedata.ens.fr/participants/challenges/11/>
+
+**Context** :
+
+- A detailed description with images illustrating the clinical context can
+   be found in the PDF attached in supplementary materials.
+
+##### Challenge goals :
+
+- The goal of this challenge is to build an image classifier to assist 
+  physicians in the screening and diagnosis of esophageal cancer. 
+  Such a tool would have a massive impact on patient management and 
+  patient lives.
+
+**Rank**:
+
+- 17/51
+
+
+
+*Pour améliorer :*
+
+- Ayant dépassé le temps que je m'étais fixé pour ce challenge, je ne mettrais pas cette méthode en application, mais je pense fortement qu'une normalisation par individu (plusieurs images par individus) plutôt que par Batch permettrai une amélioration significatif du score.
---
+++ utils/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,47 @@
+from sklearn.model_selection import train_test_split
+import os
+import shutil
+import pandas as pd
+
+
+
+def new_distrib():
+    y=pd.read_csv('DATA/X_target.csv')
+    y['patient'] = y.image_filename.apply(lambda x :x.split('.')[0].split('_')[-1])
+    sep_df = y.groupby('patient').min()
+    train,test = train_test_split(sep_df,stratify=sep_df.class_number, test_size=0.1)
+    y['set']= y.patient.apply(lambda x: 'train' if x in train.index else 'test')
+    y.class_number = y.class_number.astype(str)
+    print('Répartion : \nTrain:',sum(y.set=='train'),"Test:",sum(y.set=='test'),sep='\n')
+    ###
+    # Change Filename
+    y.image_filename = y.apply(lambda x: f'DATA/Train/{x.class_number}/{x.image_filename}',axis=1)
+    display(y[y.set=='train'].groupby('class_number').count())
+    display(y[y.set=='test'].groupby('class_number').count())
+    y.to_csv('splits.csv',index=False)
+    
+def builds_folders():
+    y=pd.read_csv('DATA/X_target.csv') 
+    train, test = train_test_split(y,test_size=0.2)
+    try:
+        os.mkdir(f'DATA/Train/')
+        for x in frozenset(y.class_number):
+            os.mkdir(f'DATA/Train/{x}')
+    except:pass
+
+    try:
+        os.mkdir(f'DATA/Test/')
+        for x in frozenset(y.class_number):
+            os.mkdir(f'DATA/Test/{x}')
+    except:pass
+
+    for idx,elem in train.iterrows():
+        curr = f'DATA/TrainingSetImagesDir/{elem.image_filename}'
+        new = f'DATA/Train/{elem.class_number}/{elem.image_filename}'
+        shutil.move(curr,new)
+        
+    for idx,elem in test.iterrows():
+        curr = f'DATA/TrainingSetImagesDir/{elem.image_filename}'
+        new = f'DATA/Test/{elem.class_number}/{elem.image_filename}'
+        shutil.move(curr,new)
+        
